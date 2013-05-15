cd44bdb2d9a368e1cb37d0b1b60c0f368c2a266a
Joakim Ekblad <aio.github@aio.nu>
2013-05-15 22:31:25 +0200

    Made documentation for testcases for Note.

--- src/NoteTest.cpp
+++ src/NoteTest.cpp
@@ -1,3 +1,9 @@
+/**
+ * @file NoteTest.cpp
+ *
+ * Test cases for Note.
+ */
+
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
@@ -5,6 +11,10 @@
 
 using ::testing::_;
 
+/**
+ * @test A new Note without constructor parameters shall instantiate a Note
+ *       with the note value set to 0 and the velocity set to 0.
+ */
 TEST(Note, Constructor_without_arguments_shall_set_note_and_velocity_to_zero) {
   Note *note = new Note();
   ASSERT_EQ(note->note, 0);
@@ -12,6 +22,10 @@ TEST(Note, Constructor_without_arguments_shall_set_note_and_velocity_to_zero) {
   delete note;
 }
 
+/**
+ * @test A new Note with constructor arguments shall set note number and
+ *       velocity accordingly.
+ */
 TEST(Note, Constructor_shall_set_note_number_and_velocity) {
   Note *note = new Note(10, 14);
   ASSERT_EQ(note->note, 10);
@@ -19,6 +33,9 @@ TEST(Note, Constructor_shall_set_note_number_and_velocity) {
   delete note;
 }
 
+/**
+ * @test set_note shall set the note value of a Note.
+ */
 TEST(Note, Note_setter_shall_set_note) {
   Note *note = new Note();
   note->set_note(10);
@@ -26,6 +43,9 @@ TEST(Note, Note_setter_shall_set_note) {
   delete note;
 }
 
+/**
+ * @test set_velocity shall set the velocity value of a Note.
+ */
 TEST(Note, Velocity_setter_shall_set_velocity) {
   Note *note = new Note();
   note->set_velocity(10);
@@ -33,12 +53,18 @@ TEST(Note, Velocity_setter_shall_set_velocity) {
   delete note;
 }
 
+/**
+ * @test get_node shall get the note value of a Note.
+ */
 TEST(Note, Note_getter_shall_get_node) {
   Note *note = new Note(10, 14);
   ASSERT_EQ(note->get_note(), 10);
   delete note;
 }
 
+/**
+ * @test get_velocity shall get the velocity of a Note.
+ */
 TEST(Note, Velocity_getter_shall_get_velocity) {
   Note *note = new Note(10, 14);
   ASSERT_EQ(note->get_velocity(), 14);
